3e6955968cfabc1ff60356f379ea6ee701d8c361
April <aprilcs@users.noreply.github.com>
2019-07-06 05:05:38 -0400

    Add files via upload

--- fig1_happiness_of_individuals.r
+++ fig1_happiness_of_individuals.r
@@ -1,9 +1,9 @@
 library(ggplot2)
-data <- read.csv("/data/data.csv", header=TRUE, sep=",", as.is=TRUE)
+data <- read.csv("data/data.csv", header=TRUE, sep=",", as.is=TRUE)
 myPlot = ggplot(data, aes(data$trade_number, 
     data$happiness_rating,colour=data$participant_name)) + 
     geom_line() + 
     geom_point()
 ggsave(
   filename = "fig1_happiness_of_individuals.png", plot = myPlot,
-  device = "png", path = "/results/")
+  device = "png", path = "results/")
--- fig2_distribution_of_happiness.r
+++ fig2_distribution_of_happiness.r
@@ -1,7 +1,7 @@
-data <- read.csv("/data/data.csv", header=TRUE, sep=",", as.is=TRUE)
+data <- read.csv("data/data.csv", header=TRUE, sep=",", as.is=TRUE)
 
 dev.new()
-png("/results/fig2_distribution_of_happiness.png")
+png("results/fig2_distribution_of_happiness.png")
 boxplot(happiness_rating ~ trade_number, data = data)
 dev.off()
 
