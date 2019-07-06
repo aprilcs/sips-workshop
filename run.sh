f5c53935fa6c7c848809957babf958763e45f284
April <aprilcs@users.noreply.github.com>
2019-07-06 11:06:13 +0200

    Delete run.sh

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-Rscript "fig1_happiness_of_individuals.r"
-Rscript "fig2_distribution_of_happiness.r"
-
-Rscript -e "rmarkdown::render('candy_trade.Rmd', 'html_document',output_dir = '../results')"
-
-
