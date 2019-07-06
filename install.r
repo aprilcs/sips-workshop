471152b2741ca9336d2e6cfb39400659fddc2a7a
April <aprilcs@users.noreply.github.com>
2019-07-06 05:01:33 -0400

    Add files via upload

---
+++ install.r
@@ -0,0 +1,6 @@
+install.packages("bitops")
+install.packages("rmarkdown")
+install.packages("caTools")
+install.packages("ggplot2")
+install.packages("knitr")
+install.packages("rprojroot")
---
+++ run.r
@@ -0,0 +1,7 @@
+# source("install.r")
+source("/code/fig1_happiness_of_individuals.r")
+source("/code/fig2_distribution_of_happiness.r")  
+
+rmarkdown::render('/code/candy_trade.Rmd', 
+                  'html_document',
+                  output_dir = '/results')
