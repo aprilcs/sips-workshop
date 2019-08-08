87649403d77c49cbb9c3d3f7fb569dd8012555a2
April <aprilcs@users.noreply.github.com>
2019-08-08 09:41:28 -0700

    Update run.r

--- code/run.r
+++ code/run.r
@@ -1,7 +1,8 @@
 source("install.r")
-source("code/fig1_happiness_of_individuals.r")
-source("code/fig2_distribution_of_happiness.r")  
 
 rmarkdown::render('code/candy_trade.Rmd', 
                   'html_document',
                   output_dir = 'results')
+
+source("code/fig1_happiness_of_individuals.r")
+source("code/fig2_distribution_of_happiness.r")  
